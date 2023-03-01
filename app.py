3f5fee62fab266aeffab91ec708d896f070e81d7
chishungu <chishungu16@gmail.com>
2023-03-01 01:26:07 -0800

    finally got everything into json

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from flask import Flask,render_template,request
+import requests
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Welcome to the the Pokemon app!" #welcome to the pokemon app
-
-@app.route('/pokemon-form.html', methods=['GET', 'POST'])
+@app.route('/pokemon', methods=['GET', 'POST'])
 def pokemon():
-    pokemon_names = ["bulbasaur", "charmander", "squirtle", "pikachu", "eevee"]
+   print(request.method)
 
-    if request.method == 'POST':
-        pokemon_names = request.form['pokemon_names']
-        response = request.get(f'https://pokeapi.co/api/v2/pokemon/{name}')
-        pokemon_data = response.json()
+   pokemon_names = ["bulbasaur", "charmander", "squirtle", "pikachu", "eevee"]
 
-        pokemon_names = pokemon_data['name']
+   pokemon_data = {} # created a empty dictionary which i will store in my pokemon data 
+
+   name = request.form.get('name')
+   for name in pokemon_names:
+    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{name}')
+    if response.status_code == 200: # if th response is 200 I want to make request to the api 
         pokemon_json = response.json()
         abilities = [ability['ability']['name'] for ability in pokemon_json['abilities']]
         sprite_url = pokemon_json['sprites']['front_shiny']
@@ -24,17 +23,29 @@ def pokemon():
         attack_base_stat = pokemon_json['stats'][1]['base_stat']
         hp_base_stat = pokemon_json['stats'][0]['base_stat']
         defense_base_stat = pokemon_json['stats'][2]['base_stat']
-   
-
-
-        return render_template('pokemon.html', name = pokemon_names, abilities = abilities,
-            sprite_url = sprite_url,
-            base_experience = base_experience,
-            attack_base_stat =attack_base_stat,
-            hp_base_stat = hp_base_stat,
-            defense_base_stat=defense_base_stat 
-            )
         
-    return render_template('pokemon-form.html')
+        #store my pokemon data into my empty dictionery 
+        pokemon_data[name] = {
+            
+            "abilities": abilities,
+            "sprite_url": sprite_url,
+            "base_experience": base_experience,
+            "attack_base_stat": attack_base_stat,
+            "hp_base_stat": hp_base_stat,
+            "defense_base_stat": defense_base_stat
+        }
+    else:
+        # If th request is 404 I want to return error message 
+        print(f'Error retrieving data for {name}: {response.status_code}')
 
+    for name, data in pokemon_data.items():
+        print(f"Pokemon: {name}")
+        print(f"Abilities: {', '.join(data['abilities'])}")
+        print(f"Sprite URL: {data['sprite_url']}")
+        print(f"Base Experience: {data['base_experience']}")
+        print(f"Attack Base Stat: {data['attack_base_stat']}")
+        print(f"HP Base Stat: {data['hp_base_stat']}")
+        print(f"Defense Base Stat: {data['defense_base_stat']}")
+        print()
+   return render_template('pokemon.html', pokemon_data = pokemon_data, name = name, data=data)
 
